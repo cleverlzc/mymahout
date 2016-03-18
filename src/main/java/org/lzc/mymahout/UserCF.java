6141f5b6d27d4c74b67947926cab4b1867846bfd
Lzc <Lzc@xjtuse-PC>
2016-03-18 12:35:52 +0800

    first commit

---
+++ src/main/java/org/lzc/mymahout/UserCF.java
@@ -0,0 +1,42 @@
+package org.lzc.mymahout;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.model.file.FileDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.model.DataModel;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+public class UserCF {
+	
+	final static int NEIGHBORHODD_NUM = 2;
+	final static int RECOMMENDER_NUM =3;
+	
+	public static void main(String[] args) throws IOException, TasteException { 
+		String file = "datafile/item.csv";
+		DataModel model = new FileDataModel(new File(file));
+		UserSimilarity user = new EuclideanDistanceSimilarity(model);
+		NearestNUserNeighborhood neighbor = new NearestNUserNeighborhood(NEIGHBORHODD_NUM,user,model);
+		Recommender r = new GenericUserBasedRecommender(model,neighbor,user);
+		LongPrimitiveIterator it = model.getUserIDs();
+		
+		while(it.hasNext()){
+			long uid = it.nextLong();
+			List<RecommendedItem> list = r.recommend(uid, RECOMMENDER_NUM);
+			System.out.printf("uid:%s",uid);
+			for (RecommendedItem ritem : list) {
+                System.out.printf("(%s,%f)", ritem.getItemID(), ritem.getValue());
+            }
+            System.out.println();
+		}
+		
+	}
+}
